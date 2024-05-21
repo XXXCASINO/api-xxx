5a3b2147a66e8bc3b171e3686f77e59872b21ca1
xxxgamblingproject <xxxgamblingproject@gmail.com>
2024-05-20 21:45:54 -0400

    Login update register etc

--- routes/login.js
+++ routes/login.js
@@ -1,21 +1,43 @@
 const login = require('express').Router();
+const mysql = require('mysql2');
+
+// Configuração da conexão ao banco de dados
+const pool = mysql.createPool({
+    host: "sql.freedb.tech",
+    user: "freedb_gambling",
+    database: "freedb_xxxdatabase",
+    password: "w%gSsa35VC46VJV",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 login.get("/login", async function (req, res) {
     const { status } = req.query;
     if (status) {
-       res.status(200).send(
-           { loginStatus: "Theres some status", status: status }
-       );
+       res.status(200).send({ loginStatus: "Theres some status", status: status });
     } else {
        res.send("No status");
     }
 });
 
 login.post("/logout", async function (req, res) {
-     
     const { status, statusName } = req.body;
-     //do something
-     res.send("Something status");
+    //do something
+    res.send("Something status");
+});
+
+// Rota para adicionar um novo usuário
+login.post("/register", function (req, res) {
+    const { name, email, password } = req.body;
+    const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    pool.query(sql, [name, email, password], function (err, result) {
+        if (err) {
+            res.status(500).send('Failed to register user: ' + err.message);
+        } else {
+            res.send({ userId: result.insertId, message: "User successfully registered" });
+        }
+    });
 });
 
 module.exports = login;
