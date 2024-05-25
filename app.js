ab48fa29630d5846a9094f24a10e40d136818692
xxxgamblingproject <xxxgamblingproject@gmail.com>
2024-05-24 23:39:24 -0400

    Middlewares

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
+
+// Middleware para analisar JSON
+app.use(express.json());
+
+// Middleware para analisar dados codificados em URL
+app.use(express.urlencoded({ extended: true }));
+
 module.exports = app;
